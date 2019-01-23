0069c3d06de6643365415f123801d9bafda0ee82
juccalle <46544460+juccalle@users.noreply.github.com>
2019-01-23 14:31:59 -0500

    login js partially done until url is given

--- js/code.js
+++ js/code.js
@@ -1,16 +1,19 @@
 var userID = 0;
 var userName = "";
 var password = "";
+//need to get the url for the site, it goes here
+//var url = '';
 
-function login()
+function signup()
 {
 
 	//make sure to store the users username and password for log in 
 	var userName = document.getElementById("userLogin").value;
 	var password = document.getElementById("userPassword").value
+ 
 }
 
-function signUp()
+function login()
 {
 	var newUserName = document.getElementById("newUserLogin").value;
 	var newPassword = document.getElementById("newUserPassword").value
@@ -25,4 +28,35 @@ function signUp()
 	{
 		alert("How can you sign in without a password? Type one in!");
 	}
+
+	var jsonPayload = '{"login" : "' + userName + '", "password" : "' + password + '"}';
+	//var url = url + '/Login.' + extension;
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, false);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+
+	try
+	{
+		xhr.send(jsonPayload);
+		
+		var jsonObject = JSON.parse( xhr.responseText );
+		
+		userId = jsonObject.id;
+		
+		if( userId < 1 )
+		{
+			document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
+			return;
+		}
+		
+
+	}
+
+	catch(err)
+	{
+		document.getElementById("loginResult").innerHTML = err.message;
+	}
+	
+
 }
+

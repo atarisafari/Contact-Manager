9b7c6ddf5f124e47d57f3edd3a688ea7e5e7bfeb
JarvisEQ <lfjarvis@gmail.com>
2019-01-31 15:15:13 -0500

    function validateLogin

--- js/backend.js
+++ js/backend.js
@@ -6,10 +6,49 @@ function validateLogin(JSONpackage){
 	var userCreds = JSON.parse(JSONpackage);
 	//obejct includes userName, password strings
 	
-	//get the password
-	//how do????
-	var password ;
+	//get connection to the server
+	var mysql = required('mysql');
+	var con = mysql.createConnection({
+		host: //need host
+		user: //need user
+		password: //need password
+	});
 	
+	//trying connection
+	con.connection(function(err){
+		if err{
+			throw err;
+		}
+	});
+	
+	
+	
+	var id = '';
+	
+	//querry the password
+	con.query(	'SELECT user_id FROM user_info WHERE username = ' + userCreds.userName + 'AND passwordHash =  ' userCred.passHash + ' LIMIT 1', (err, id));
+	
+	//check to see if there was an error
+	if(err){
+		
+		//if err, send user id of 0
+		var newPackage =  '{"id" : "' +  0}';
+	} else {
+	
+		//else, send back the user id
+		var newPackage = '{"id" : "' +  id};'
+	}
+	
+	//send it out
+	try
+	{
+		xhr.send(jsonPayload);
+	} catch(err){
+		
+		
+	}
+	//ending connection
+	con.end(err);
 }
 
  

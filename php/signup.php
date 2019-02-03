1af4e6bbf09626c690d1d4b49d34055e7ffe2183
Ethan Finlay <efinclarke1993@gmail.com>
2019-02-02 19:44:43 -0500

    Update and rename addUser.php to signup.php

---
+++ php/signup.php
@@ -0,0 +1,21 @@
+<?php
+
+include('establishConn.php');
+
+//Prepare and bind
+$sql = $conn->prepare("INSERT INTO user_info (first_name, last_name, username, passwordHash) VALUES(?, ?, ?, ?)");
+$sql->bind_param("ssss", $firstname, $lastname, $username, $password);
+
+/*
+
+Get data from the JSON post
+
+Set parameters and execute
+$firstname = 
+$lastname = 
+$username =
+$password =
+$sql->execute
+*/
+
+?>

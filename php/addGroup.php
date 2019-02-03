bd39b871077905f27683d0243b545eee2542053a
stevelouis4848 <33739350+stevelouis4848@users.noreply.github.com>
2019-02-02 20:02:17 -0500

    fixed bind error

--- php/addGroup.php
+++ php/addGroup.php
@@ -1,14 +1,13 @@
 <?php
   include('establishConn.php);
-  Session_start();
 
-  /*
-    Get The json from the javascript and put the information into the groupName and userId variable
-  */
+  
+  //  Get The json from the javascript and put the information into the groupName and userId variable
+ 
 
   // prepare and bind
   $sql = $conn->prepare("INSERT INTO groups (group_name, user_id) VALUES (?, ?, ?)");
-  $sql->bind_param("sss", $groupName, $userId);
+  $sql->bind_param("si", $groupName, $userId);
   
   $sql->execute();
   

e2b27a2d538665ba0888feaf8b3d65416155963a
Ethan Finlay <efinclarke1993@gmail.com>
2019-02-03 00:01:49 -0500

    Update addGroup.php

--- php/addGroup.php
+++ php/addGroup.php
@@ -1,9 +1,8 @@
 <?php
+
   include('establishConn.php);
 
-  
-  /*Get The json from the javascript and put the information into the groupName and userId variable
-  $in = json_decode(file_get_contents('php://input'), true);*/
+  $in = json_decode(file_get_contents('php://input'), true);
 
   // prepare and bind
   $sql = $conn->prepare("INSERT INTO groups (group_name, user_id) VALUES (?, ?, ?)");

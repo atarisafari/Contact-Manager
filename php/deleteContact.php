4153ce76140cda52a44a2f655ca32af4e31bc84d
stevelouis4848 <33739350+stevelouis4848@users.noreply.github.com>
2019-02-04 15:36:07 -0500

    basic delete by contact id

--- php/deleteContact.php
+++ php/deleteContact.php
@@ -1,3 +1,17 @@
 <?php
 
 include('establishConn.php');
+
+$sql = $conn->prepare("DELETE FROM contact WHERE contact_id = ?");
+$sql->bind_param('i', $contact_id);
+
+//Receive JSON package from POST and assign to variables
+$input = json_decode(file_get_contents('php://input'));
+
+//Set parameters
+$contact_id = $input->contact_id;
+
+$sql->execute(); 
+$sql->close();
+
+?>

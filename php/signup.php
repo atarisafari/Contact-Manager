6f4248acdfcbe4588431ad18568f3252d10896fd
Ethan Finlay <efinclarke1993@gmail.com>
2019-02-05 14:28:49 -0500

    Update signup.php

--- php/signup.php
+++ php/signup.php
@@ -2,24 +2,35 @@
 
 include('establishConn.php');
 
-//Prepare and bind
-$sql = $conn->prepare("INSERT INTO user_info (first_name, last_name, username, passwordHash) VALUES(?, ?, ?, ?)");
-$sql->bind_param("ssss", $firstname, $lastname, $username, $password);
+//Prepare and bind for insertion
+$sql = $conn->prepare("INSERT INTO user_info (first_name, last_name, username, passwordSalt passwordHash) VALUES(?, ?, ?, ?, ?)");
+$sql->bind_param("sssss", $firstname, $lastname, $username, $passwordSalt, $passwordHash);
 
-/*
+$test = $conn->prepare("SELECT user_id FROM user_info WHERE username = ?)
+$test->bind_param("s", $username);
 
-Get data from the JSON post
-Check if username is taken
+//Get data from the JSON post
+$input = json_decode(file_get_contents('php://input), true);
 
-Set parameters and execute
-$firstname = 
-$lastname = 
-$username =
-$password =
-$sql->execute();
-*/
+//Set parameters and execute
+$firstname = $input["first_name"];   
+$lastname = $input["last_name"];
+$username = $input["username"];
+$passwordSalt = $input["passwordSalt"];
+$passwordHash = $input["passwordHash"];
+  
+$test->execute();
+$result = $test->get_result();
+
+if($result->num_rows > 0){
+  //Error, username taken
+}
+
+else
+  $sql->execute();
 
 $sql->close();
+$test->close();
 $conn->close();
 
 

7544542efd396fa4085ca0c36c04e73cc1efafbd
Fernando Trevino <trevino.96@hotmail.com>
2019-02-05 19:35:47 -0500

    signUp

--- js/index.js
+++ js/index.js
@@ -34,6 +34,41 @@ fucntion doSignUp(){
 	//make sure to store the users username and password for log in 
 	var userName = document.getElementById("userLogin").value;
 	var password = document.getElementById("userPassword").value;
+	var passwordConfirm = document.getElementById("form-password-confirm").value;
+	if (password !== passwordConfirm) {
+		document.getElementById('passwordCompareAndCreateResult').innerHTML = "Your passwords do not match. Please try again.";
+		return;
+	}
+
+	password = md5(password);
+
+	document.getElementById("passwordCompareAndCreateResult").innerHTML = "";
+
+	// replace with appropriate varaible names
+	var payload = '{"username" : "' + userName + '", "password" : "' + password + '"}';
+
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", baseURL + "/AddUser.php", false);
+	xhr.setRequestHeader("Content-type", "application/json; charset = UTF-8");
+
+	try {
+		xhr.send(payload);
+
+		var data = JSON.parse(xhr.responseText);
+		var error = data.error;
+
+		if(error !== "") {
+			document.getElementById('passwordCompareAndCreateResult').innerHTML = error;
+			return;
+		}
+
+		// return the to login page
+		window.location.href = "http://cop4331-2.com/";
+	}
+	catch(error) {
+		// include result of creation in HTML
+		document.getElementById('passwordCompareAndCreateResult').innerHTML = error.message;
+	}
 }
 
 function doLogin() {

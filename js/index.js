708bfe024de25f59b761911a93df6e597d62aa86
Fernando Trevino <trevino.96@hotmail.com>
2019-02-07 16:30:05 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -31,7 +31,7 @@ $(document).ready(function() {
 	});
 });
 
-fucntion doSignUp(){
+function doSignUp(){
 	//make sure to store the users username and password for log in 
 	var userName = document.getElementById("usernameSignUp").value;
 	var password = document.getElementById("passwordSignUp").value;

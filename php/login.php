5ee4f24e7796e1907aba8c3359075991f9147b85
Ethan Finlay <efinclarke1993@gmail.com>
2019-02-08 09:12:06 -0500

    Update login.php

--- php/login.php
+++ php/login.php
@@ -1,12 +1,34 @@
  <?php
+
    include("establishConn.php");
 
-   session_start();
-
-   $_SESSION["user_id"] = 
+   $sql = $conn->prepare("SELECT * FROM user_info WHERE username=? and passwordHash=?");
+   $sql->bind_param("ss", $username, $passwordHash);
   
+   $input = json_decode(file_get_contents('php://input'), true);
+
+   $username = $input["username"];
+   $passwordHash = $input["passwordHash"];
+
+   $sql->execute();
+   $response = $sql->get_result();
+
+   if($response->num_rows == 0)
+   {
+      $payload='{"user_id":0, "username":"", "password_hash":"", "error": "'.$error.'"}';
+      header('Content-type: application/json');
+      echo $payload
+      return;
+   }
+
+   $retData = $response->fetch_all();
+   $payload = '{"user_id":' . $retData[0][0] . ',"username":"' . $retData[0][1] . '","password_hash":"' . $retData[0][2] . '"}';
+   header('Content-type: application/json');
+   echo $payload;
+
 
    $conn->close();
+   $sql->close();
 
 ?>
   

2511c0af6c940ad06d9611dce801799c8f71b9bc
Jiayi Zhang <35317400+jiayizhg@users.noreply.github.com>
2019-02-07 19:16:32 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -119,7 +119,7 @@ function doLogin() {
 
   // Setup the JSON payload to send to the API
 	var jsonPayload = '{"username" : "' + username + '", "passwordHash" : "' + password + '"}';
-  console.log("JSON Payload: " + jsonPayload);
+	console.log("JSON Payload: " + jsonPayload);
 
 	var xhttp = new XMLHttpRequest();
 	xhttp.open("POST", baseURL + "/Login.php", false);
@@ -127,10 +127,10 @@ function doLogin() {
 
 	try {
 		xhttp.send(jsonPayload);
-    console.log("***" + xhttp.responseText);
+		console.log("***" + xhttp.responseText);
 
 		var data = JSON.parse(xhttp.responseText);
-    console.log('current user:' + data.results);
+		console.log('current user:' + data.results);
 		userID = data.user_id;
 
 		if(userID == 0) {
@@ -153,104 +153,102 @@ function doLogout() {
 }
 
 function addContact() {
-  // Get from the HTML fields
-   var firstname = document.getElementById('firstName').value;
-   var lastname = document.getElementById('lastName').value;
-   var phone_number = document.getElementById('phoneNumber').value;
-   var email = document.getElementById('email').value;
-   var birth_date = document.getElementById('birthDate').value;
-   var address = document.getElementById('address').value;
+	// Get from the HTML fields
+	var firstname = document.getElementById('firstName').value;
+   	var lastname = document.getElementById('lastName').value;
+   	var phone_number = document.getElementById('phoneNumber').value;
+  	var email = document.getElementById('email').value;
+  	var birth_date = document.getElementById('birthDate').value;
+   	var address = document.getElementById('address').value;
 
-   if(!firstname | !lastname) {
-     console.log("Must fill out firstname and lastname in order to add a contact");
-     var errorMessage = document.getElementById("loginResult");
-     errorMessage.innerHTML = "Must fill out firstname and lastname in order to add a contact";
-     return;
-   }
+	if(!firstname | !lastname) {
+		console.log("Must fill out firstname and lastname in order to add a contact");
+		var errorMessage = document.getElementById("loginResult");
+		errorMessage.innerHTML = "Must fill out firstname and lastname in order to add a contact";
+		return;
+	}
+	var jsonPayload = '{"last_name" : "' + last_name + '", "first_name" : "' + first_name + '", "phone_number" : "' + phone_number 
+	+ '", "email_address" : "' + email_address + '", "birth_date" : "' + birth_date + '", "address" : "' + address + '", "user_id" : "' + userID + '"}';
 
-   var jsonPayload = '{"last_name" : "' + last_name + '", "first_name" : "' + first_name + '", "phone_number" : "' + phone_number
-   + '", "email_address" : "' + email_address + '", "birth_date" : "' + birth_date + '", "address" : "' + address + '", "user_id" : "' + userID + '"}';
+   	var xhr = new XMLHttpRequest();
+   	xhr.open("POST", baseURL + "/addContact.php", true);
+   	xhr.setRequestHeader("Content-type", "application/json; charset = UTF-8");
 
-   var xhr = new XMLHttpRequest();
-   xhr.open("POST", baseURL + "/addContact.php", true);
-   xhr.setRequestHeader("Content-type", "application/json; charset = UTF-8");
-
-   try {
-   	xhr.onreadystatechange = function(){
-      // Operation is complete
-   		if (xhr.readyState === 4){
-  			 document.getElementById('firstName').value = '';
-  			 document.getElementById('lastName').value = '';
-  			 document.getElementById('phoneNumber').value = '';
-  			 document.getElementById('email').value = '';
-  			 document.getElementById('birthDate').value = '';
-         		 document.getElementById('address').value = '';
-   		}
-   	}
-      xhr.send(jsonPayload);
-   }
-   catch(error) {
-      document.getElementById('addContactResult').innerHTML = error.message;
-   }
+   	try {
+   		xhr.onreadystatechange = function(){
+			// Operation is complete
+			if (xhr.readyState === 4){
+				document.getElementById('firstName').value = '';
+				document.getElementById('lastName').value = '';
+				document.getElementById('phoneNumber').value = '';
+				document.getElementById('email').value = '';
+				document.getElementById('birthDate').value = '';
+				document.getElementById('address').value = '';
+			}
+		}
+		xhr.send(jsonPayload);
+	}
+	catch(error) {
+		document.getElementById('addContactResult').innerHTML = error.message;
+	}
 }
 
 function searchContacts() {
-   clearContacts();
-   var target = document.getElementById('search-username').value;
-   lastSearch = target;
+	clearContacts();
+   	var target = document.getElementById('search-username').value;
+   	lastSearch = target;
+	
+	var payload = '{"search" : "' + target + '", "uID" : "' + userID + '"}';
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", baseURL + "/SearchContact.php", true);
+	xhr.setRequestHeader("Content-type", "application/json; charset = UTF-8");
 
-   var payload = '{"search" : "' + target + '", "uID" : "' + userID + '"}';
-
-   var xhr = new XMLHttpRequest();
-   xhr.open("POST", baseURL + "/SearchContact.php", true);
-   xhr.setRequestHeader("Content-type", "application/json; charset = UTF-8");
-
-   try {
-   	xhr.send(payload);
-  	xhr.onload = function () {
-   	    if (xhr.readyState === xhr.DONE) {
-
-		var data = JSON.parse(xhr.responseText);
-		searchResult = data.result;
-		for (str in searchResult){
-			contact = searchResult[str].split(" | ");
-			addContactRow(contact[1], contact[2], contact[3], contact[4], contact[0]);
+	try {
+		xhr.send(payload);
+		xhr.onload = function () {
+			if (xhr.readyState === xhr.DONE) {
+				var data = JSON.parse(xhr.responseText);
+				searchResult = data.result;
+				for (str in searchResult){
+					contact = searchResult[str].split(" | ");
+					addContactRow(contact[1], contact[2], contact[3], contact[4], contact[0]);
+				}
+			}
 		}
 	}
-      }
-    }
-    catch(error) {
+	catch(error) {
 		// include result of login in HTML
 		document.getElementById('searchResultText').innerHTML = error.message;
-    }
+	}
 }
 
 
 function deleteContacts()
 {
-  var nodeList = document.getElementsByClassName("");
-
-  if(!nodeList)
-  {
-    console.log("table hasnt loaded yet");
-    return;
-  }
-  for(var i = 0; i < nodeList.length; i++)
-  {
-    if(nodeList[i].checked)
-    {
-      var value = nodeList[i].parentNode.id;
-      console.log("value is : " + value);
-      var contactId = '"id" : "' + value + '"' ;
-      var functionName = '"function" : "deleteContact",';
-      var jsonPayload = "{"+functionName+contactId+"}";
-      CallServerSide(jsonPayload);
-    }
-    if(i == nodeList.length - 1) break;
-  }
-  var errorMessage = document.getElementById("loginResult");
-  errorMessage.innerHTML = "";
-
-  // To prevent page refresh
-  return false;
+	var nodeList = document.getElementsByClassName(""); 
+	
+	if(!nodeList)
+  	{
+		console.log("table hasnt loaded yet");
+		return;
+  	}
+	
+	for(var i = 0; i < nodeList.length; i++)
+	{
+		if(nodeList[i].checked)
+		{
+			var value = nodeList[i].parentNode.id;
+			console.log("value is : " + value);
+			var contactId = '"id" : "' + value + '"' ;
+			var functionName = '"function" : "deleteContact",';
+			var jsonPayload = "{"+functionName+contactId+"}";
+			CallServerSide(jsonPayload);
+		}
+		if(i == nodeList.length - 1) break;
+	}
+	
+	var errorMessage = document.getElementById("loginResult");
+	errorMessage.innerHTML = "";
+	// To prevent page refresh
+	return false;
 }

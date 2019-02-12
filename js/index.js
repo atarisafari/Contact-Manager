2c3af5e23251f6cea6ec6640b255e463b970add0
Fernando Trevino <trevino.96@hotmail.com>
2019-02-11 19:41:34 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -327,7 +327,7 @@ function deleteContact(contactID, row) {
 	xhr.open("POST", baseURL + "/deleteContact.php", true);
 	xhr.setRequestHeader("Content-type", "application/json; charset = UTF-8");
 	xhr.send(JSON.stringify(payload));
-	document.getElementById("contactsTable").deleteRow(row.parentNode.parentNode.rowIndex);
+	document.getElementById("contactsTable").deleteRow(row.parentNode.rowIndex);
 	//searchContacts();
 }
 
